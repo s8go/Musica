f22a20dfad556f49f9ee7f8e41f20d157339b94b
segun <soshice@gmail.com>
2022-10-16 03:12:11 +0100

    project setup and tailwind installation done 100

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold text-red-700 text-center w-1/3 m-auto mt-4 bg-gray-700">
+    Hello world!
+  </h1>
   );
 }
 
