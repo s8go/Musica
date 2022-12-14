71e2c88862a8eb8dfed4b35331bb256f712b4f6e
segun <soshice@gmail.com>
2022-12-14 12:44:02 +0100

    scrollbar removed successfully

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'header-image': "url('/src/images/Vector.png')",
+       "desktop-background": "url('/src/images/desktop-bg.svg')"
+      },
+     backgroundColor:{
+        "play": "rgb(0, 0, 0, 0.5)"
+      },
+      
+    },
+  },
+    plugins: [
+      require('tailwindcss-no-scrollbar')
+  ],
+}
